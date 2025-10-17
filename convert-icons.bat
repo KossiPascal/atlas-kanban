f51e83314c5fae55b751295e119b65d2e01ab3c6
KossiPascal <kossi.tsolegnagbo@gmail.com>
2025-10-17 11:22:35 +0000

    Initial Commit

---
+++ convert-icons.bat
@@ -0,0 +1,15 @@
+
+convert ./icon.svg -resize 16x16 ./favicon-16x16.ico
+convert ./icon.svg -resize 32x32 ./favicon-32x32.ico
+convert ./icon.svg -resize 48x48 ./icons/icon-48x48.png
+convert ./icon.svg -resize 64x64 ./icons/icon-64x64.png 
+convert ./icon.svg -resize 72x72 ./icons/icon-72x72.png 
+convert ./icon.svg -resize 96x96 ./icons/icon-96x96.png 
+convert ./icon.svg -resize 128x128 ./icons/icon-128x128.png 
+convert ./icon.svg -resize 144x144 ./icons/icon-144x144.png 
+convert ./icon.svg -resize 152x152 ./icons/icon-152x152.png 
+convert ./icon.svg -resize 180x180 ./icons/icon-180x180.png
+convert ./icon.svg -resize 192x192 ./icons/icon-192x192.png 
+convert ./icon.svg -resize 256x256 ./icons/icon-256x256.png 
+convert ./icon.svg -resize 384x384 ./icons/icon-384x384.png 
+convert ./icon.svg -resize 512x512 ./icons/icon-512x512.png
